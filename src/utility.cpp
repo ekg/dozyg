cd43ef1587c6abd57afdfb4cd7013e6846aff5f8
Erik Garrison <erik.garrison@gmail.com>
2020-01-15 15:28:08 +0100

    utility functions for filling a file

---
+++ src/utility.cpp
@@ -0,0 +1,13 @@
+#include "utility.hpp"
+
+namespace gyeet {
+
+void allocate_file(const std::string& fname, size_t size) {
+    FILE* f = fopen(fname.c_str(), "w");
+    int r = fseek(f, size-1, SEEK_SET);
+    char x = '\0';
+    size_t w = fwrite(&x, 1, 1, f);
+    r = fclose(f);
+}
+
+}
---
+++ src/utility.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdio>
+#include <string>
+
+namespace gyeet {
+
+void allocate_file(const std::string& fname, size_t size);
+
+}
