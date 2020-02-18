17e4b0e84c105cd5bf8828a7685e28ca99947819
Erik Garrison <erik.garrison@gmail.com>
2020-02-18 12:01:50 +0100

    show chains

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -112,17 +112,13 @@ std::string map_seq(
                                max_chain_gap,
                                mismatch_rate);
     std::stringstream ss;
-    /*
     for (auto& chain : query_chains) {
         write_chain_gaf(ss, chain, index, name, query.length());
     }
-    */
     auto query_superchains = superchains(query_chains, index.kmer_length, mismatch_rate);
-    /*
     for (auto& superchain : query_superchains) {
         write_superchain_gaf(ss, superchain, index, name, query.length());
     }
-    */
     uint64_t up_to = std::min(align_best_n, (uint64_t)query_superchains.size());
     for (uint64_t i = 0; i < up_to; ++i) {
         auto& superchain = query_superchains[i];
